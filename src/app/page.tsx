b65d1e55ce489120a3ea1078c15890943bdc39a3
maurolonmartin <maurolonmartin@hotmail.com>
2024-12-09 19:40:55 -0500

    Fix the problem in the build in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import HomeClient from "../components/HomeComponent/HomeClient"; 
-import { fetchTransactions } from "@/services/apiService";
 
 export default async function Home() {
-  const transactions = await fetchTransactions();
 
   return (
     <div className="container">
       <main>
-        <HomeClient transactions={transactions.data} />
+        <HomeClient />
       </main>
     </div>
   );
